1db25c3547735a3a19f0fae4e3895ff3ad80fbbf
annadacenko <148321947+annadacenko@users.noreply.github.com>
2023-10-20 00:36:37 +0300

    Add files via upload

---
+++ UnitTest.Api.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class TestRegistration:
+    def test_registration(self):
+        body = {"username": "test@test.com", "password": "Password"}
+        response = requests.post(
+            "https://stores-tests-api.herokuapp.com/register", json=body
+        )
+        assert response.status_code == 201
+        assert response.json().get("message") == "User created successfully."
+        assert response.json().get("uuid")
+        assert isinstance(response.json().get("uuid"), int)
+        print(response.text)
